592f278ea8e7c6411edc3526f84eba9a643f1bf0
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-03-06 11:42:09 +0900

    リストに表示するデータを扱う配列を追加

--- ListsInUICollectionViewSample/ListsInUICollectionViewSample/ViewController.swift
+++ ListsInUICollectionViewSample/ListsInUICollectionViewSample/ViewController.swift
@@ -17,6 +17,9 @@ class ViewController: UIViewController {
         case main
     }
     
+    // 表示するデータ
+    let colorNames = ["red", "blue", "green", "yellow", "black", "white"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
